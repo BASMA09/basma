96d1eff2dd7df1d8f2debad4f6910774ef6fd47c
BASMA09 <b1ssmaelhamdi@gmail.com>
2024-11-19 14:46:29 +0100

    ccccc

---
+++ EXERCISE.js
@@ -0,0 +1,112 @@
+
+
+// let nombre = 7;
+// if (nombre % 2 == 0) {
+//     console.log("le nombre est pair");
+// } else {
+//     console.log("le nombre est impair");
+// }
+// let X = 7;
+// if (X % 2 == 0) {
+//     console.log(X+"  "+"le nombre est pair");
+// } else {
+//     console.log(X+"  "+"le nombre est impair");
+// }
+// let age = 19;
+// if (age>=18) {
+//     console.log(age+" "+"la personne est majeur");
+// }else{
+//     console.log(age+" "+"la personne est mineur");
+// }
+
+//  let jours = 4;
+//  switch( jours){
+//   case 1 :
+//  console.log("c'est Lundi") ;  
+//  break;
+//  case 2:
+//  console.log("c'est Mardi");
+//  break;
+//  case 3:
+//  console.log("c'est Mercredi");
+//  break;
+//  case 4:
+//  console.log("c'est jeudi");
+//  break;
+//  case 5:
+// console.log("c'est Vendredi");
+// break;
+// case 6:
+// console.log("cest Samedi");
+// break;
+// case 7:
+// console.log("cest Dimanche"); 
+// break;}  
+
+// let montant = 400;
+// if (montant > 100) {
+//     let reduction = montant * 0.10; // 10% de réduction
+//     let montantApresReduction = montant - reduction;
+//     console.log(montantApresReduction)
+
+// } else {
+//     let montantApresReduction = 400;
+// }
+
+
+
+// let note= 2;
+// if (90<=note<=100){
+//  console.log("une note 2excelente");} 
+//  else if(70<=note<=89)
+//     {console.log("une note bien");
+// } else if(50<=Note<=69)
+// {console.log("une note moyen");}
+// else if 
+// (note<50) 
+// {console.log("c'est insuffisant");}
+// else
+// {console.log("les appreciation valables sontde 0 à100");}
+ 
+
+// EXERCICE2
+for (a=2;a<=20;a+=2)
+    console.log("le nombre = "+a)
+
+for(x=1;x<=5;x++){
+    let caree =x*x
+console.log("le caree ="+x*x)}
+
+
+
+for (x=10;x>=1;x--)
+    console.log(+x)
+//exercise
+for(x=1;x<=5;x++){
+    let nbr =x*3
+    console.log(nbr)}
+
+//exercice 1
+function saluer(){
+ console.log("Bonjour tout le monde")   
+}saluer()
+//exercice2
+function saluerNom (nom){
+    console.log("bonjour "+nom);
+}
+ saluerNom("basma"); 
+ //Exercice3
+ 
+ function addition (a,b){
+   return a+b; 
+ }  
+ console.log(addition(1,2));
+
+
+//EXERCICE4:
+function affichermessage(message){
+    console.log("votre message"+  message)
+}
+affichermessage(" :  incroyable ")
+
+
---
+++ exercise 2.js
@@ -0,0 +1,7 @@
+var  x ;
+console.log(typeof (x))
+console.log("la valeur de x : "+x)
+let y=null;
+console.log(typeof (y));
+console.log("la valeur de y : "+y)
+console.log("la comparison"+(x==y)   
